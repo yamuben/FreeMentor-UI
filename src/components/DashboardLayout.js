08190f785690867d8b62959ff22a2841b3abcb32
yamuben <yamubbenjamin@gmail.comn>
2021-09-06 21:30:01 +0200

    about session

--- src/components/DashboardLayout.js
+++ src/components/DashboardLayout.js
@@ -30,7 +30,8 @@ const DashboardLayout =({children})=> {
           <div className="logo" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1" icon={<UserOutlined />}>
-              Sessions
+                <Link to="/dashboard">   Sessions      </Link>
+              
             </Menu.Item>
             <Menu.Item key="2" icon={<VideoCameraOutlined />}>
                 <Link to="/dashmentors">   Mentors      </Link>
