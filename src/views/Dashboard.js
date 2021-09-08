394ec2b30b4e078e861f7db81112c01c95061cd0
yamuben <yamubbenjamin@gmail.comn>
2021-09-08 21:52:22 +0200

    viewing session profile

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,102 +1,127 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-// import './index.css';
+import './index.css';
 import data from "../assets/constants/sessions.json";
 
 import Session from "../components/Session";
+import SessionProfile from "../components/SessionProfile";
 import DashboardLayout from "../components/DashboardLayout";
 import {
-    Modal,Tag,Space,
-    Button,Table
-  } from 'antd';
-
-
-
-const columns = [
-    {
-      title: 'Session Title',
-      dataIndex: 'title',
-      key: 'title',
-      render: text => <a>{text}</a>,
-    },
-    {
-      title: 'Mentor',
-      dataIndex: 'mentor',
-      key: 'mentor',
-
-      render: mentor => <a>{mentor.firstName}</a>,
-    },
-    {
-      title: 'Start Time',
-      dataIndex: 'timeToStart',
-      key: 'timeToStart',
-    },
-    {
-      title: 'End Time',
-      dataIndex: 'timeToEnd',
-      key: 'timeToEnd',
-    },
-    {
-      title: 'Status',
-      key: 'status',
-      dataIndex: 'status',
-      render: tag => {
-            let color = tag === "pending" ? 'black' : tag==="accept"? "green" : "red";
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-         
-            },
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-        <Space size="middle">
-          <a>View</a>
-          <a>Delete</a>
-          <a>Edit</a>
-        </Space>
-      ),
-    },
-  ];
+    Modal, Tag, Space,
+    Button, Table, Drawer
+} from 'antd';
 
 
 
 
-const Dashboard =()=>{
+
+const Dashboard = () => {
+
+    const [session,setSession]=useState({});
     const [isModalVisible, setIsModalVisible] = useState(false);
 
+    const [visible, setVisible] = useState(false);
+
     const showModal = () => {
-      setIsModalVisible(true);
+        setIsModalVisible(true);
     };
-  
+
+
     const handleOk = () => {
-      setIsModalVisible(false);
+        setIsModalVisible(false);
     };
-  
+
     const handleCancel = () => {
-      setIsModalVisible(false);
+        setIsModalVisible(false);
     };
 
-    return(
-<DashboardLayout>
 
-<Button onClick={showModal}>Create Session</Button>
+    const onClose = () => {
+        setVisible(false);
 
-<Table columns={columns} dataSource={data} />
+    }
 
-<Modal title="New Session" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-<div style={{padding:"30px"}}>
-<Session/>
+    const columns = [
+        {
+            title: 'Session Title',
+            dataIndex: 'title',
+            key: 'title',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: 'Mentor',
+            dataIndex: 'mentor',
+            key: 'mentor',
 
-</div>
-    </Modal>
+            render: mentor => <a>{mentor.firstName}</a>,
+        },
+        {
+            title: 'Start Time',
+            dataIndex: 'timeToStart',
+            key: 'timeToStart',
+        },
+        {
+            title: 'End Time',
+            dataIndex: 'timeToEnd',
+            key: 'timeToEnd',
+        },
+        {
+            title: 'Status',
+            key: 'status',
+            dataIndex: 'status',
+            render: tag => {
+                let color = tag === "pending" ? 'black' : tag === "accept" ? "green" : "red";
+                return (
+                    <Tag color={color} key={tag}>
+                        {tag.toUpperCase()}
+                    </Tag>
+                );
 
-</DashboardLayout>
+            },
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <Space size="middle">
+                    <a onClick={()=>{setVisible(true); setSession(record)}}>View</a>
+                    <a>Delete</a>
+                    <a>Edit</a>
+                </Space>
+            ),
+        },
+    ];
+
+
+
+    return (
+        <>
+            <DashboardLayout>
+
+                <Button onClick={showModal}>Create Session</Button>
+
+                <Table columns={columns} dataSource={data} />
+
+                <Modal title="New Session" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                    <div style={{ padding: "30px" }}>
+                        <Session />
+
+                    </div>
+                </Modal>
+
+            </DashboardLayout>
+            
+            <Drawer
+                width={640}
+                placement="right"
+                closable={false}
+                onClose={onClose}
+                visible={visible}
+            >
+            <SessionProfile session={session} />
+            </Drawer>
+        </>
     )
 }
 
