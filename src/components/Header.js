d668d26529700fa4d0bffb8cc6b62d6ed1c30513
yamuben <yamubbenjamin@gmail.comn>
2021-09-15 21:53:10 +0200

    login logout ft

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 
 import {Menu,Modal} from "antd";
 import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import "./index.css";
 import 'antd/dist/antd.css';
 import {Link} from "react-router-dom";
-
 import SigninForm from "./SigninForm";
+
+import decode from "../utils/tokenDecoder";
 
 
 const { SubMenu } = Menu;
 const Header = ()=>{
 
+
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [token, setToken]=useState(null);
+  const [dataFromToken,setDataFromToken]=useState({});
+
+
+  const handleDecode=()=>{
+    setDataFromToken(decode(token));
+  }
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -32,6 +41,10 @@ const Header = ()=>{
     console.log('click ', e);
     setCurrent(e.key);
   };
+useEffect(() => {
+ setToken( localStorage.getItem("freeMentor_token"));
+
+})
 
     return(
       <>
@@ -45,7 +58,7 @@ const Header = ()=>{
         <Link to="/home">   <h1 style={{color:"green", fontSize:"30px",fontWeight:"bolder"}}> FREE MENTOR</h1>
         </Link>
 
-      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" style={{margin:"0px 0px 0px 40%"}}>
+      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" style={{margin:"0px 0px 0px 35%",width:"50%"}}>
         <Menu.Item key="home" >
    
         <Link to="/">
@@ -55,9 +68,13 @@ const Header = ()=>{
           <Link onClick={handleClick} to="/allmentors">
          All Mentors</Link>
         </Menu.Item>
-        <Menu.Item key="login" onClick={showModal}>
+      {  !token?
+        (<Menu.Item key="login" onClick={showModal}>
          Login
-        </Menu.Item>
+        </Menu.Item>):
+        (<Menu.Item key="logout" onClick={()=>{localStorage.removeItem("freeMentor_token")}}>
+        <Link to="/home">Logout</Link>
+        </Menu.Item>)}
       </Menu>
        </div>
        </>
